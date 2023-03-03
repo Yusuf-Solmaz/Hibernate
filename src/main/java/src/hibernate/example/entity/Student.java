959cbd2dd2c4d042ebaf6f9b5172335ca1fd3f5e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-03 22:18:49 +0300

    Adding Entity (Student)

---
+++ src/main/java/src/hibernate/example/entity/Student.java
@@ -0,0 +1,49 @@
+package src.hibernate.example.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public  Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+}
