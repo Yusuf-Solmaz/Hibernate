07b3721dea5ab52c685cce464c7f9590b6b83af2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-10 21:14:59 +0300

    Adding new Entity for ManytoOne Relation

---
+++ src/main/java/src/hibernate/example/entity/Course.java
@@ -0,0 +1,41 @@
+package src.hibernate.example.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,
+            CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", teacher=" + teacher.getFirstName() +
+                '}';
+    }
+}
